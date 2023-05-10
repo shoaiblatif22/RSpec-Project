a4c073a2dd4ff7c8c9ea12b0a1c78b0b6d7ab1d6
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-10 11:11:38 +0100

    Added in second test

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Counter do
     result = new_counter.report()
     expect(result).to eq "Counted to 5 so far."
   end
+  it "The counter should be counting the next three numbers" do
+    new_counter = Counter.new()
+    new_counter.add(10)
+    new_counter.add(15)
+    new_counter.add(5)
+    result = new_counter.report()
+    expect(result).to eq "Counted to 30 so far."
+  end
 end
