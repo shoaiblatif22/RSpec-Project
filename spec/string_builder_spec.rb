b4cf0e72f67a08f362490b2ef1645175cc435a17
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-10 10:59:39 +0100

    Added in a second test to test the concatenation of two strings

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe StringBuilder do
     expect(result_output).to eq "world"    
   end
 
-  it "" 
+  it "concatenates two strings and returns length" do
+    string_builder = StringBuilder.new()
+    string_builder.add ("testing")
+    string_builder.add ("rspec")
+    result_size = string_builder.size()
+    result_output = string_builder.output()
+    expect(result_size).to eq 12
+    expect(result_output).to eq ("testingrspec")
+  end 
 end
 
